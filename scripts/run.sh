01367f7bcc617dcb70232d1a08a063440fb9455a
toxdes <malivp3494@gmail.com>
2025-12-15 21:59:39 +0530

    chore: dbg: use temp file for quick runs

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,12 @@
 printf "[RUN]\n"
 set -xe
 
-DEBUG_FLAG="-L debug"
-# DEBUG_FLAG=""
-
-./build/glesha add $DEBUG_FLAG -p aws -a targz -c ../glesha-secrets/config.json ~/Downloads
+# DEBUG_FLAG="-L debug"
+DEBUG_FLAG=""
+dd if=/dev/urandom of=output.dat  bs=100M  count=40
+./build/glesha add $DEBUG_FLAG -p aws -a targz -c ../glesha-secrets/config.json output.dat
 ./build/glesha run $DEBUG_FLAG -j 4 1
+rm output.dat
 # ./build/glesha add -p aws -L debug ~/pro/scripts
 # ./build/glesha -i ~/pro/scripts provider":"aws",-p aws -c ../glesha-secrets/config.json --assume-yes
 # ./build/glesha help config
